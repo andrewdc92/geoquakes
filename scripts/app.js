2cb1ce0dd4b0c314e48af9dd8a09ced62956b58a
andrewdc92 <andrew.cordivari@gmail.com>
2016-08-24 15:14:48 -0700

    console log works, returning an object

--- scripts/app.js
+++ scripts/app.js
@@ -2,9 +2,38 @@
 var weekly_quakes_endpoint = "http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson";
 var $quakesList;
 var map;
-var template;
+
 
 $(document).on("ready", function() {
 
+  var source=$('#quakes-template').html();
+  var template =Handlebars.compile(source);
+
+  $.ajax ({
+    method: "GET",
+    url: weekly_quakes_endpoint,
+    // data:
+    dataType:'json',
+    success: renderQuakeData,
+    error: onError,
+
+  })
+
+    function renderQuakeData(quakeResults) {
+      console.log("received results:", quakeResults);
+    }
+
+    function onError(xhr, status, errorThrown) {
+      alert("Sorry, there was a problem!");
+      console.log("Error: " + errorThrown);
+      console.log("Status: " + status);
+      console.dir(xhr);
+    }
 
 });
+
+// // pass in data to render in the template
+// var trackHtml = template({ quakesList: trackResults });
+//
+// // append html to the view
+// $results.append(trackHtml);
